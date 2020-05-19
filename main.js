dbc3684ffbbc04a788a7a87598f52bf753c17dc4
Martina L Deterville <donnamartina78@example.com>
2020-05-19 06:11:21 -0400

    second commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const AllDiceRolls = document.querySelector("#Show-All-Rolls")
+const NumberDice = document.querySelector ("#numberDice")
+const Roll = document.querySelector("#Rolls")
+const Restart = document.querySelector("#Restarts")
+
+let DieRolls = [ShowAllRolls, numberDice, Rolls, Restarts] 
+
+
+Roll.addEventListener("click",function(){
+    Roll.innerHTML = Roll = 0
+    if (Roll <=6)
+       console.log (Roll)
+       Roll +=0
+    }
+    console.log ("CLICK")
+})
+
+
+
+while (indexDieRolls)
+
+    
+Show-All-Rolls.addEventListener("click",function(){
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
