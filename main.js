e57c648f4b009832fc83a517010079232885406c
Martina L Deterville <donnamartina78@example.com>
2020-05-25 21:33:44 -0400

    second commit

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
+const NumberDice = document.querySelector ("#numberDice");
+const Roll = document.querySelector("#Roll");
+const totalRolls = document.querySelector("#die-roll-total");
+const ShowAllRolls= document.querySelector("#show-all-rolls");
+const eachRoll = document.querySelector("#each-roll");
 
-const AllDiceRolls = document.querySelector("#Show-All-Rolls")
-const NumberDice = document.querySelector ("#numberDice")
-const Roll = document.querySelector("#Rolls")
-const Restart = document.querySelector("#Restarts")
-
-let DieRolls = [ShowAllRolls, numberDice, Rolls, Restarts] 
-index (2) = 
-
-Roll.addEventListener("click",function(){
-    Roll.innerHTML = Roll = 0
-    if (Roll <=6)
-       console.log (Roll)
-       Roll +=0
-    })
-Console.log ("CLICK")
+let dieRolls = [];
 
 
-while (indexDieRolls)
-  
-Show-All-Rolls.addEventListener("click",function(){
+Roll.addEventListener ("click",function(){
+    let numberOfDiceRolls = Number(NumberDice.value);
+    var i = 0;
+    dieRolls = [];
+    while(i < numberOfDiceRolls){
+        //randomly generate a die face between 1 and 6
+        //add result to the array
+        dieRolls.push(Math.floor(Math.random() * 6 + 1));
+        i = i + 1;
+    }
 
+    //add all the elements of the array
+    let sum = 0;
+    var j = 0;
+    while(j < dieRolls.length){
+        sum = (sum + dieRolls[j]);
+        j = j + 1;
+    }
+
+    totalRolls.textContent = sum;
 
 })
 
-
-
-
-
+ShowAllRolls.addEventListener("click", function(){
+    eachRoll.innerHTML = dieRolls;
+})
 
 
 
